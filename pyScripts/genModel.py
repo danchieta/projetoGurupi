decc3b1770aff75c21eed67baa083cfb73c09eac
unknown <david.dca@SRPA2013050776.pf.gov.br>
2017-05-04 16:58:09 -0300

    corrigido uso de transpose no lugar de reshape

--- pyScripts/genModel.py
+++ pyScripts/genModel.py
@@ -4,12 +4,15 @@ from PIL import Image
 
 def vecOfSub(shp):
 	x,y = np.meshgrid(range(shp[0]),range(shp[1]))
-	x = np.transpose(x,(1,x.size),order='f')
-	y = np.transpose(y,(1,y.size),order='f')
-	return np.array([x,y])
+	x = np.reshape(x,(1,x.size),order='f')
+	y = np.reshape(y,(1,y.size),order='f')
+	return np.array(x,y)
 
 
-f = 0.5
+f = 0.5 # fator de subamostragem
+theta = np.pi/6 #angulo de rotacao
+
+R = np.array([[np.cos(theta) , np.sin(theta)],[-np.sin(theta) , np.cos(theta)] ])
 
 #r = vecOfSub((3,3))
 
@@ -20,4 +23,4 @@ dd = np.round(d*f).astype('int')
 
 
 imgr = Image.fromarray(img).convert('RGB')
-imgr.save('res2.bmp')
+#imgr.save('res2.bmp')

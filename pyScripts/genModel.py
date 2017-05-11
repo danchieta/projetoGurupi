5363d95946495c8bac48ac833bbf714bb2a7d8a3
david <danchieta@github.com>
2017-05-11 12:08:27 -0300

    expansão da psf

--- pyScripts/genModel.py
+++ pyScripts/genModel.py
@@ -9,19 +9,40 @@ def vecOfSub(shp):
 	y = np.reshape(y,(1,y.size),order='f').squeeze()
 	return np.array([x,y])
 
+def psf(i, j, gamma, theta, s, shapei, shapeo, v):
+	N = shapei.prod()
+	M = shapeo.prod()
 
-f = 0.5 # fator de subamostragem
-theta = np.pi/6 #angulo de rotacao
+	R = np.array([[np.cos(theta) , np.sin(theta)],[-np.sin(theta), np.cos(theta)]]) 
 
-R = np.array([[np.cos(theta) , np.sin(theta)],[-np.sin(theta) , np.cos(theta)] ])
+	vec_j = vecOfSub(shapeo)
+	vec_i = vecOfSub(shapei)
 
-#r = vecOfSub((3,3))
+	v = np.repeat(v,M).reshape(2,M)
+	s = np.repeat(s,M).reshape(2,M)
+
+	vec_u = np.dot(R, (vec_j-v))+v+s
+
+	W = -np.linalg.norm(vec_i[:,i] - vec_u[:,j])**2/gamma**2
+
+	return W
+
 
 img = np.array(Image.open('../testIMG/imteste.png').convert('L'))
+
+f = 0.5 # fator de subamostragem
+gamma = 2.0
+
 
 d = np.array(img.shape)
 dd = np.round(d*f).astype('int')
 
+s = (0,0)
+v = (dd/2.0).round()
+
+theta = np.pi/6 #angulo de rotacao
+
+W = psf(0,0,gamma, theta, s, d, dd, v)
 
 imgr = Image.fromarray(img).convert('RGB')
 #imgr.save('res2.bmp')

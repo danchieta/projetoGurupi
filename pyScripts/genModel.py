0392133e374de6a5de4f4846b0d0135207712350
unknown <david.dca@SRPA2013050776.pf.gov.br>
2017-05-16 17:16:13 -0300

    ainda presisa ajustar a função psf

--- pyScripts/genModel.py
+++ pyScripts/genModel.py
@@ -30,10 +30,7 @@ def psf(j, gamma, theta, s, shapei, shapeo, v):
 
 	vec_u = np.dot(R, (vec_j-v))+v+s
 
-	vec_W = np.array([])
-	
-	for k in range(N):
-		vec_W = np.append(vec_W, -np.linalg.norm(vec_i[:,k] - vec_u[:,j])**2/gamma**2)
+	vec_W = -np.linalg.norm(vec_i - vec_u[:,j].reshape(2,1), axis=0)**2/gamma**2
 
 	return vec_W/vec_W.sum()
 
@@ -44,24 +41,24 @@ d = np.array(img.shape) #dimensoes da imagem de entrada
 
 img = img.reshape(d.prod(),1)
 
-f = 0.5 # fator de subamostragem
-gamma = 2.0 # tamanho da funcao de espalhamento de ponto
+f = 0.1 # fator de subamostragem
+gamma = 0 # tamanho da funcao de espalhamento de ponto
 
 dd = np.round(d*f).astype('int') #dimensoes da imagem de saida
 
 s = (0,0) #deslocamento da imagem
 v = (dd/2.0).round() #centro da imagem 
 
-theta = np.pi/6 #angulo de rotacao
+theta = 0 #angulo de rotacao
 
 y = np.zeros(dd.prod())
-W = psf(0,gamma, theta, s, d, dd, v) #funcao de espalhamento de ponto
+#W = psf(0,gamma, theta, s, d, dd, v) #funcao de espalhamento de ponto
 
 
-#for i in range(dd.prod()):
-#	W = psf(i,gamma, theta, s, d, dd, v) #funcao de espalhamento de ponto
-#	y[i] = np.dot(W,img)
-
-
-#imgr = Image.fromarray(img).convert('RGB')
-#imgr.save('res2.bmp')
+for i in range(dd.prod()):
+	W = psf(i,gamma, theta, s, d, dd, v) #funcao de espalhamento de ponto
+	y[i] = np.dot(W,img)
+	print 100.0*i/dd.prod()
+	
+imgr = Image.fromarray(y.reshape(dd)).convert('RGB')
+imgr.save('res2.bmp')

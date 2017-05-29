30e80ab1c69094d03a590c3261708846fdf8583d
david <danchieta@github.com>
2017-05-29 13:29:24 -0300

    tentando usar o modulo scipy.sparse para reduzir o custo do algorítmo

--- pyScripts/srModel.py
+++ pyScripts/srModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import scipy
+from scipy import sparse
 
 def vecOfSub(shp):
 	x,y = np.meshgrid(range(shp[0]),range(shp[1]))
@@ -19,21 +19,33 @@ def priorDist(shapei, A = 1, r = 1):
 		np.meshgrid(vec_i[1],vec_i[1])[0] - np.meshgrid(vec_i[1],vec_i[1])[1],
 		]).astype(np.float16)
 
-	return A*np.exp(-(np.linalg.norm(Z, axis=0)**2)/r**2.)
+	return sparse.csc_matrix(A*np.exp(-(np.linalg.norm(Z, axis=0)**2.)/r**2.))
 
-inFolder = '../degradedImg/' #diretorio de saida
-
-filename = np.genfromtxt(inFolder + 'paramsImage.csv', dtype=str, skip_header = 1, usecols = 0, delimiter = ';' )
-s = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = [1,2], delimiter = ';' )
-theta = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = 3, delimiter = ';' )
-gamma = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = 4, delimiter = ';' )[0]
-f = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = 5, delimiter = ';' )[0]
-
-img = np.array(Image.open(inFolder + filename[1]).convert('L'))
-d_in = np.round(img.shape/f).astype(int)
-
-Z = priorDist(d_in)
+d_in  = (20,20)
+Z = priorDist(d_in, A = 0.04, r=1.0)
 
 plt.spy(Z)
 plt.show()
+
+
+
+
+
+
+
+# inFolder = '../degradedImg/' #diretorio de saida
+
+# filename = np.genfromtxt(inFolder + 'paramsImage.csv', dtype=str, skip_header = 1, usecols = 0, delimiter = ';' )
+# s = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = [1,2], delimiter = ';' )
+# theta = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = 3, delimiter = ';' )
+# gamma = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = 4, delimiter = ';' )[0]
+# f = np.genfromtxt(inFolder + 'paramsImage.csv', skip_header = 1, usecols = 5, delimiter = ';' )[0]
+
+# img = np.array(Image.open(inFolder + filename[1]).convert('L'))
+# d_in = np.round(img.shape/f).astype(int)
+
+# Z = priorDist(d_in)
+
+# plt.spy(Z)
+# plt.show()
 

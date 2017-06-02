912404befed53f534b52c76d9f7814a403ea57cd
david <danchieta@github.com>
2017-06-01 23:52:16 -0300

    usando o modulo scipy.sparse para calcular a psf

--- pyScripts/genModel.py
+++ pyScripts/genModel.py
@@ -1,6 +1,7 @@
 #este modulo contem as funcoes referentes ao modelo de observacao
 
 import numpy as np
+from scipy import sparse
 
 # function return vector of subscripts
 def vecOfSub(shp):
@@ -9,17 +10,17 @@ def vecOfSub(shp):
 	y = np.reshape(y,(1,y.size),order='f').squeeze()
 	return np.array([x,y])
 
-def psf(j, gamma, theta, s, shapei, shapeo, v):
+def psf(gamma, theta, s, shapei, shapeo, v):
 	# numero de pixels em cada imagem
-	N = shapei.prod()
-	M = shapeo.prod()
+	N = np.prod(shapei)
+	M = np.prod(shapeo)
 
 	# Matriz de rotacao
 	R = np.array([[np.cos(theta) , np.sin(theta)],[-np.sin(theta), np.cos(theta)]]) 
 
 	#Vetores de subscritos das imagens de saida e entrada
-	vec_j = vecOfSub(shapeo)
-	vec_i = vecOfSub(shapei)
+	vec_j = vecOfSub(shapeo).astype(np.float16)
+	vec_i = vecOfSub(shapei).astype(np.float16)
 	
 	v = v.reshape(2,1) #centro da imagem como vetor coluna
 	s = np.array(s).reshape(2,1) #deslocamento como vetor coluna
@@ -27,22 +28,23 @@ def psf(j, gamma, theta, s, shapei, shapeo, v):
 	# Rotacao e deslocamento de cada pixel da imagem HR
 	vec_u = np.dot(R, (vec_j-v))+v+s
 
-	# gerando uma linha da funcao de espalhamento de ponto
-	vec_W = np.exp(-np.linalg.norm(vec_i - vec_u[:,j].reshape(2,1), axis=0)**2/gamma**2)
+	# gerando a matriz da funcao de espalhamento de ponto
+	vec_W = np.array([vec_u[0][np.newaxis].T - vec_i[0],
+		vec_u[1][np.newaxis].T - vec_i[1]])
+	vec_W = np.exp(-np.linalg.norm(vec_W, axis = 0)**2./gamma**2.)
 
 	# retorna linha da PSF normalizada
-	return vec_W/vec_W.sum()
+	return sparse.csc_matrix(vec_W/vec_W.sum(axis = 1)[np.newaxis].T)
 
 def degradaImagem(img, gamma, theta, s, f, sigma = 4):
 	d = np.array(img.shape) #dimensoes da imagem de entrada
 	img = img.reshape(d.prod(),1)
 	dd = np.round(d*f).astype('int') #dimensoes da imagem de saida
 	v = (dd/2.0).round() #centro da imagem 
-	y = np.zeros(dd.prod()) + np.random.randn(dd,1)*sigma #Vetor imagem resultante
+	y = np.zeros(dd.prod()) + np.random.randn(dd.prod(),1)*sigma #Vetor imagem resultante
 
-	for i in range(dd.prod()):
-		W = psf(i,gamma, theta, s, d, dd, v) #gera uma linha imgrfuncao de espalhamento de ponto
-		y[i] = np.dot(W,img) #aplicacao da fucao de espalhamento de ponto
+	W = psf(gamma, theta, s, d, dd, v) #gera uma linha imgrfuncao de espalhamento de ponto
+	y = W*img #aplicacao da fucao de espalhamento de ponto
 		#print 100.0*i/dd.prod()
 
 	return y.reshape(dd)

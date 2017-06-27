1d20923b15388bb1b910fc449b3bdf9629869b9b
david <danchieta@github.com>
2017-06-27 12:09:37 -0300

    Mais teste na verossimilhança dos parametros

--- pyScripts/srteste1.py
+++ pyScripts/srteste1.py
@@ -9,7 +9,7 @@ csv2 = 'globalParams.csv'
 D = srModel.Data(inFolder, csv1, csv2)
 D.setWindowLR((8,8))
 
-E1 = srModel.Estimator(D)
+E1 = srModel.ParameterEstimator(D)
 
 L = [E1.likelihood(D.gamma, D.theta, D.s)]
 
@@ -19,12 +19,12 @@ expNum = 15
 acertos = 0
 
 for i in range(expNum):
-	#gamma = np.random.rand(num)*2+1
-	theta = (np.random.rand(D.N,num)*4-2)*np.pi/180
-	s = np.random.rand(2,D.N,num)*4-2
+	gamma = D.gamma
+	theta = (np.random.rand(D.N,num)*16-8)*np.pi/180
+	s = np.random.rand(2,D.N,num)*8-4
 
 	for k in range(num):
-		L.append(E1.likelihood(2, theta[:,k], s[:,:,k]))
+		L.append(E1.likelihood(gamma, theta[:,k], s[:,:,k]))
 
 	if np.argmax(L) == 0:
 		acertos += 1	

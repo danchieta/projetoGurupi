eb9d4acfd0fc288d870ddba8cb1e3528c17e63f4
david <danchieta@github.com>
2017-09-25 23:47:31 -0300

    Refinando o procedimento para otimizar os parâmetros.

--- pyScripts/parameterOptimization.py
+++ pyScripts/parameterOptimization.py
@@ -17,7 +17,7 @@ D = srModel.Data(inFolder, csv1, csv2)
 
 # use just a small window of the image to compute parameters
 # reducing computational cost
-windowshape = (10,10)
+windowshape = (9,9)
 D.setWindowLR(windowshape)
 
 # create parameter estimator object
@@ -28,29 +28,47 @@ gamma0 = 2 # tamanho da funcao de espalhamento de ponto
 s0 = np.random.rand(2,D.N)*8-4 #deslocamento da imagem
 theta0 = (np.random.rand(D.N)*16-8)*np.pi/180 #angulo de rotacao (com variancia de pi/100)
 
-#initial vector with the parameters
-v0 = srModel.vectorizeParameters(theta0, s0)
+# FIRST STEP: Optimize shifts
+# ===========================
+# initial vector with shifts
+v0 = srModel.vectorizeParameters(s0)
 
-# vector with true parameters
+# vector with true shifts
+vtrue = srModel.vectorizeParameters(D.s)
+
+# norm of the error before algorithm
+err_before = np.linalg.norm(v0-vtrue)
+print 'Error before shifts optimization:', err_before
+
+# use cg to optimize shifts
+v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0, theta0), callback = func, maxiter = 10)
+
+# recover s from the vector
+s_a = srModel.unvectorizeParameters(v, D.N, ('s'))
+# norm of the error after algorithm
+err_after = np.linalg.norm(v-vtrue)
+print 'Error after shifts optimization:', err_after
+
+# STEP TWO: Optimize shifts AND theta
+# ===================================
+# Build a new initial vector using the shifts from the previous step
+v0 = srModel.vectorizeParameters(theta0, s_a)
+
+# vector with true shifts and angles
 vtrue = srModel.vectorizeParameters(D.theta, D.s)
 
 # norm of the error before algorithm
 err_before = np.linalg.norm(v0-vtrue)
-print 'Error before algorithm:', err_before
+print 'Error before shifts AND theta optimization:', err_before
 
-# save likelihood of the initial vector
-P = [E2.vectorizedLikelihood(v0, 1.0, gamma = 2)]
-
-# use cg to maximize likelihood
-v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), callback = func, maxiter = 32)
-
-# compute likelihood of the result
-P.append(E2.vectorizedLikelihood(v, 1, gamma0))
+# Optimize shifts and rotations
+v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), callback = func, maxiter = 15)
 
 # norm of the error after algorithm
 err_after = np.linalg.norm(v-vtrue)
 print 'Error after algorithm:', err_after
 
+# Unpack parameters 
 theta_a, s_a = srModel.unvectorizeParameters(v, D.N, ('theta', 's'))
 
 err_theta = np.linalg.norm(D.theta - theta_a)
@@ -60,6 +78,7 @@ err_s = np.linalg.norm(D.s - s_a, axis=0)
 print 'Mean of the error of s', err_s.mean()
 print err_s[np.newaxis].T
 
-plt.figure(1)
-plt.plot(P)
+plt.figure()
+plt.scatter(D.s[0,:], D.s[1,:], marker = 'o', label = 'True shifts')
+plt.scatter(s_a[0,:], s_a[1,:], marker = '^', label = 'Estimated shifts')
 plt.show()

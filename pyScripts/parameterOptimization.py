7e5ea4fef1143d0b6825d339ad6b3ff20a46038e
david <danchieta@github.com>
2017-09-27 00:17:04 -0300

    Ainda trabalhando na otimização dos parâmetros.

--- pyScripts/parameterOptimization.py
+++ pyScripts/parameterOptimization.py
@@ -23,10 +23,12 @@ D.setWindowLR(windowshape)
 # create parameter estimator object
 E2 = srModel.ParameterEstimator(D)
 
+P = list()
+
 # defining initial parameters
 gamma0 = 2 # tamanho da funcao de espalhamento de ponto
-s0 = np.random.rand(2,D.N)*8-4 #deslocamento da imagem
-theta0 = (np.random.rand(D.N)*16-8)*np.pi/180 #angulo de rotacao (com variancia de pi/100)
+s0 = np.random.rand(2,D.N)*4-2 #deslocamento da imagem
+theta0 = (np.random.rand(D.N)*8-4)*np.pi/180 #angulo de rotacao (com variancia de pi/100)
 
 # FIRST STEP: Optimize shifts
 # ===========================
@@ -40,8 +42,13 @@ vtrue = srModel.vectorizeParameters(D.s)
 err_before = np.linalg.norm(v0-vtrue)
 print 'Error before shifts optimization:', err_before
 
+# Save the likelihood of the initial vector
+P.append(E2.vectorizedLikelihood(v0, 1, gamma0, theta0))
+
 # use cg to optimize shifts
-v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0, theta0), callback = func, maxiter = 10)
+v, L, _, _, _ = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0, theta0), callback = func, maxiter = 10, full_output = 1)
+
+P.append(L)
 
 # recover s from the vector
 s_a = srModel.unvectorizeParameters(v, D.N, ('s'))
@@ -62,7 +69,9 @@ err_before = np.linalg.norm(v0-vtrue)
 print 'Error before shifts AND theta optimization:', err_before
 
 # Optimize shifts and rotations
-v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), callback = func, maxiter = 15)
+v, L, _, _, _ = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), callback = func, maxiter = 15, full_output = 1)
+
+P.append(L)
 
 # norm of the error after algorithm
 err_after = np.linalg.norm(v-vtrue)
@@ -78,7 +87,15 @@ err_s = np.linalg.norm(D.s - s_a, axis=0)
 print 'Mean of the error of s', err_s.mean()
 print err_s[np.newaxis].T
 
-plt.figure()
+P = -np.abs(np.array(P))
+
+plt.figure(1)
 plt.scatter(D.s[0,:], D.s[1,:], marker = 'o', label = 'True shifts')
 plt.scatter(s_a[0,:], s_a[1,:], marker = '^', label = 'Estimated shifts')
+plt.legend(loc = 4)
+plt.title('True shifts versus estimated shifts')
+plt.show()
+
+plt.figure(2)
+plt.plot(P)
 plt.show()

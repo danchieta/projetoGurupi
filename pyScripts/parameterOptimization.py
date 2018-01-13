25b9998deb618f9e3477f4123bf419f980c35008
david <danchieta@github.com>
2018-01-12 21:03:51 -0300

    Mudanças menores

--- pyScripts/parameterOptimization.py
+++ pyScripts/parameterOptimization.py
@@ -3,7 +3,7 @@
 import numpy as np
 import scipy.optimize
 import matplotlib.pyplot as plt
-# The modules below are mine
+# The modules below where written by me
 import srModel
 import vismodule
 
@@ -26,12 +26,12 @@ def func_step(v):
 		args = (gamma0, theta0)
 	elif len(params) == 2:
 		args = (gamma0,)
-#	gradients.append(np.linalg.norm(scipy.optimize.approx_fprime(v, E2.vectorizedLikelihood, epsilon, 1.0, *args)))
+	gradients.append(np.linalg.norm(scipy.optimize.approx_fprime(v, E2.vectorizedLikelihood, epsilon, 1.0, *args)))
 	
 	# save current likelihood
 	P.append(E2.vectorizedLikelihood(v, 1, gamma0, theta0))
 	print 'current error [' + str(len(norms)-1) + '] =', norms[-1]
-#	print 'current gradient [' + str(len(gradients)-1) + '] =', gradients[-1]
+	print 'current gradient [' + str(len(gradients)-1) + '] =', gradients[-1]
 
 inFolder = '../degradedImg/'
 csv1 = 'paramsImage.csv'
@@ -53,8 +53,8 @@ gamma0 = 2 # tamanho da funcao de espalhamento de ponto
 s0 = np.zeros((2,D.N)) #deslocamento da imagem
 theta0 = np.zeros(D.N) #angulo de rotacao (com variancia de pi/100)
 
-min_grad = 9e5 #CG algorithm should stop if gradient runs below this
 epsilon = 1.49012e-8 # norm of the step used in gradient approximation
+maxiter = [40, 40] # maximum number of iterations in the optimization algorithm for each step
 
 # FIRST STEP: Optimize shifts
 # ===========================
@@ -67,11 +67,15 @@ vtrue = srModel.vectorizeParameters(D.s)
 # run function on initial vector to save error norm, gradient and function evaluation
 func_step(v0)
 
+# min_grad = 0.4*np.linalg.norm(scipy.optimize.approx_fprime(v0, E2.vectorizedLikelihood,
+#	epsilon, -1, gamma0, theta0))#CG algorithm should stop if gradient runs below this
+min_grad = 1e-5
+
 # norm of the error before algorithm
 print 'Error before shifts optimization:', norms[0]
 
 # use cg to optimize shifts
-v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0, theta0), callback = func_step, epsilon = epsilon, maxiter = 30, gtol = min_grad)
+v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0, theta0), callback = func_step, epsilon = epsilon, maxiter = maxiter[0], gtol = min_grad)
 
 # recover s from the vector
 s_a = srModel.unvectorizeParameters(v, D.N, ('s'))
@@ -84,13 +88,17 @@ print 'Error after shifts optimization:', norms[-1]
 v0 = srModel.vectorizeParameters(theta0, s_a)
 params = ('theta','s') # parameters included in vector for optimization
 
+#min_grad = 0.4*np.linalg.norm(scipy.optimize.approx_fprime(v0, E2.vectorizedLikelihood,
+	#epsilon, -1, gamma0))#CG algorithm should stop if gradient runs below this
+min_grad = 1e-5
+
 # vector with true shifts and angles
 vtrue = srModel.vectorizeParameters(D.theta, D.s)
 
 print 'Error before shifts AND theta optimization:', norms[-1]
 
 # Optimize shifts and rotations
-v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), callback = func_step, epsilon = epsilon, maxiter = 40, gtol = min_grad)
+v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), callback = func_step, epsilon = epsilon, maxiter = maxiter[1], gtol = min_grad)
 
 # END OF CONJUGATE GRADIENTS ALGORITHM
 # ====================================
@@ -100,7 +108,7 @@ v = scipy.optimize.fmin_cg(E2.vectorizedLikelihood, v0, args = (-1, gamma0), cal
 print 'Error after algorithm:', norms[-1]
 P = np.array(P) # make array of list P
 norms = np.array(norms)
-# gradients = np.array(gradients)
+gradients = np.array(gradients)
 
 # Unpack parameters 
 theta_a, s_a = srModel.unvectorizeParameters(v, D.N, ('theta', 's'))
@@ -118,13 +126,13 @@ err_s = np.linalg.norm(D.s - s_a, axis=0)
 print 'Mean of the error of s', err_s.mean()
 print err_s[np.newaxis].T
 
-vismodule.saveData(g0 = gamma0, s0 = theta0, t0 = theta0, sa = s_a, ta = theta_a, P = P, norms = norms, ws = np.array(windowshape))
+vismodule.saveData(g0 = gamma0, s0 = theta0, t0 = theta0, sa = s_a, ta = theta_a, P = P, norms = norms, ws = np.array(windowshape), maxiter = np.array(maxiter))
 
 fig1, ax1 = vismodule.compareParPlot(s_a, D.s, np.abs(D.theta-theta_a)*180/np.pi, titlenote = u'[Máxima verossimilhança]' )
 fig2, ax2 = vismodule.compareParPlot(s_min, D.s, np.abs(D.theta-theta_min)*180/np.pi, titlenote = u'[Menor erro encontrado]')
 
 fig3, ax3 = vismodule.progressionPlot(P, norms, E2.likelihood(D.gamma, D.theta, D.s))
 plt.show()
-# fig4, ax4 = vismodule.simplePlot((gradients,), title = u'Progressão da norma do gradiente', xlabel = u'Iteração')
+fig4, ax4 = vismodule.simplePlot((gradients,), title = u'Progressão da norma do gradiente', xlabel = u'Iteração')
 
-vismodule.saveFigures(fig1, fig2, fig3)
+vismodule.saveFigures(fig1, fig2, fig3, fig4)
